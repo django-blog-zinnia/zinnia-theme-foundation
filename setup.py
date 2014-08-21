4f2c3b4c8228417685c0183dd8ef200c01eb618f
Laville Augustin <augustin.laville@gustavi.net>
2014-08-21 15:38:24 +0200

    Add CHANGELOG

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name = 'zinnia-theme-foundation',
-    version = '1.0',
+    version = '1.0.1',
     packages = ['zinnia_foundation'],
     include_package_data = True,
     license = 'GPL',
