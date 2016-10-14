293b79ae070cf58521484f68b77eac8b9618f7ef
Fantomas42 <fantomas42@gmail.com>
2016-10-14 14:29:50 +0200

    Review the packaging

--- setup.py
+++ setup.py
@@ -1,27 +1,25 @@
-import os
+"""Setup script of zinnia-theme-foundation"""
+from setuptools import find_packages
 from setuptools import setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
-    README = readme.read()
+import zinnia_foundation
 
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name = 'zinnia-theme-foundation',
-    version = '1.0.1',
-    packages = ['zinnia_foundation'],
-    include_package_data = True,
-    license = 'GPL',
-    description = 'A theme with Zurb Foundation for django-blog-zinnia.',
-    long_description = README,
-    url = 'https://github.com/gustavi/zinnia-theme-foundation',
-    author = 'gustavi',
-    author_email = 'augustin.laville@gustavi.net',
-    platforms = 'any',
-    install_requires = [
-        'django-blog-zinnia',
-    ],
-    classifiers = [
+    name='zinnia-theme-foundation',
+    version=zinnia_foundation.__version__,
+    license=zinnia_foundation.__license__,
+
+    description='A theme with Zurb Foundation for django-blog-zinnia.',
+    long_description=open('README.md').read(),
+    keywords='django, blog, weblog, zinnia, theme, skin, foundation',
+
+    url=zinnia_foundation.__url__,
+    author=zinnia_foundation.__author__,
+    author_email=zinnia_foundation.__email__,
+
+    packages=find_packages(exclude=['demo_zinnia_foundation']),
+    classifiers=[
         'Framework :: Django',
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
@@ -32,6 +30,7 @@ setup(
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
-    zip_safe = False
-)
 
+    zip_safe=False,
+    include_package_data=True
+)
